f47ec0b1b8b3957368329dce054a1cb964a78da4
Maxie Bernt <maxie.bernt@hotmail.de>
2020-03-16 19:26:47 +0100

    Menüs, menüs

---
+++ Assets/Scripts/UI/StartMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public static StartMenu Instance;
+    public List<SubMenuView> SubViews = new List<SubMenuView>();
+
+    public bool IsMenuActive { get; set; }
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(Instance);
+        }
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            ExitGame();
+        }
+    }
+
+    public void OpenCredits()
+    {
+        SubViews[0].Open();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/SubMenuView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SubMenuView : MonoBehaviour
+{
+    public GameObject View;
+
+    public void Close()
+    {
+        View.SetActive(false);
+        StartMenu.Instance.IsMenuActive = false;
+    }
+
+    public void Open()
+    {
+        View.SetActive(true);
+        StartMenu.Instance.IsMenuActive = true;
+    }
+}
