37cf798641db063422eebf7ac7665ef1e3e10464
Tankbart <leif-eric.nordmann@web.de>
2020-03-17 19:20:03 +0100

    Ressource and WorkerManager firstShell

---
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,293 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    /*
+     * Für Maxie :*
+     * 
+     * Danke für alle die Viele Arbeit 
+     * 
+     * Diese Klasse ist wie folgt auf gebaut:
+     *  - erstens Singelton
+     *  - zweitens attribute geordnet nach den vier Ressourcen 
+     *  - Faith (0-100);
+     *      - steigt immer
+     *      - uFaith= unused Faith (0-Faith).
+     *          - kann verbraucht werden
+     *          - Faith kann am schrein gegen Segen (Blutregen etc getauscht werden)getauscht werden
+     *          - Faith bleibt auf der selben höhe aber uFaith wird verringert
+     *  - Watchscore (0-100)
+     *      - steigt immer
+     *      - 4 Levels. 
+     *      - 0 = start Level
+     *      - 1 = observation Level
+     *      - 2 = attack Level
+     *      - 3 U LOOSE
+     *  - Money (0-X)
+     *      - kann verbraucht werden
+     *  - Worker(0-75)
+     *      - kann verbraucht werden
+     *      - fWorker = Free Worker(0-Worker).
+     *    
+     * -Als nächstes wird in den Abschnitten nach Konstant, 
+     *      lauf variable (also die die angezeigt werden müssen), 
+     *      sub attribute (steigungs rate etc.) 
+     *      und als letztes Stellschrauben 
+     * 
+     */
+
+
+    //Standart shit
+    private static ResourceManager _instance;
+    [HideInInspector]
+    public static ResourceManager Instance { get { return _instance; } }
+    private WorkerManager wm;
+
+    //--------------------------------------------------------------------------------------------------------
+    //Faith
+    //--------------------------------------------------------------------------------------------------------
+
+    //Konstanten
+    public readonly int WIN_FAITH = 100;  
+
+    //MainVariables
+    [HideInInspector]
+    public float faith;
+    [HideInInspector]
+    public float faithGrowth;
+    [HideInInspector]
+    public float faithGrowthModifyer;
+    [HideInInspector]
+    public float uFaith;
+
+    //subvariables
+    [HideInInspector]
+    public float faithPW;
+    [HideInInspector]
+    public float faithPerSacrifice;
+    [HideInInspector]
+    public float watchscoreGrowthPerSacrifice;
+    //Stellschrauben
+    public float standardFaithPW;
+    public float standardFaithPerSacrifice;
+    public float standardWatchscoreGrowthPerSacrifice;
+    [Space(20)]
+
+    //--------------------------------------------------------------------------------------------------------
+    //Watchscore
+    //--------------------------------------------------------------------------------------------------------
+    //Konstanten
+    public readonly int LOOSE_WATCHSCORE = 100;
+    public readonly int LEVEL_ONE_WATCHSCORE = 25;
+    public readonly int LEVEL_TWO_WATCHSCORE = 75;
+
+    //MainVariables
+    [HideInInspector]
+    public float watchscore;
+    [HideInInspector]
+    public float watchscoreGrowth;
+    [HideInInspector]
+    public float watchscoreGrowthModifyer;
+    [HideInInspector]
+    public float moneyLoss;
+    [HideInInspector]
+    public float workerKillRate;
+    private float killRateCounter;
+
+    //subvariables
+    [HideInInspector]
+    public float watchscoreDividerNormal;
+    [HideInInspector]
+    public float watchscoreDividerMiddle;
+    [HideInInspector]
+    public float watchscoreDividerHigh;
+
+    [HideInInspector]
+    public float moneyLossDividerMiddle;
+    
+
+    [HideInInspector]
+    public float wKillrateDividerHigh;
+    [HideInInspector]
+    public float kostPerMilitaWorker;
+    //Stellschrauben
+    public float standardWatchscoreGrowth;
+    public float standardMoneyLossOnLevelTwo;
+    public float standardWorkerKillRateOnLevelThree;
+
+    [Space(10)]
+    public float standardWatchscoreDividerNormal;
+    public float standardWatchscoreDividerMiddle;
+    public float standardWatchscoreDividerHigh;
+    [Space(10)]
+    public float standardMoneyLossDividerMiddle;
+    [Space(10)]
+    public float standardWKillrateDividerHigh;
+    public float standardKostPerMilitaWorker;
+    [Space(20)]
+    //--------------------------------------------------------------------------------------------------------
+    //Money
+    //--------------------------------------------------------------------------------------------------------
+    //Konstanten
+    //MainVariables
+    [HideInInspector]
+    public float money;
+    [HideInInspector]
+    public float roundedMoney;
+    [HideInInspector]
+    public float moneyGrowth;
+    [HideInInspector]
+    public float moneyGrowthModifyer;
+    //subvariables
+    [HideInInspector]
+    public float moneyPWNormal;
+    [HideInInspector]
+    public float moneyPWMiddle;
+    [HideInInspector]
+    public float moneyPWHigh;
+
+    [HideInInspector]
+    public float faithPMWMiddle;
+
+    [HideInInspector]
+    public float watchscoreGrowthPMWHigh;
+    //Stellschrauben
+    public int startMoney;
+    [Space(10)]
+    public float standardMoneyPWNormal;
+    public float standardMoneyPWMiddle;
+    public float standardMoneyPWHigh;
+    [Space(10)]
+    public float standardFaithPMWMiddle;
+    public float standardwatchscoreGrowthPMWHigh;
+    [Space(20)]
+    //--------------------------------------------------------------------------------------------------------
+    //Workers
+    //--------------------------------------------------------------------------------------------------------
+    //Konstanten
+    //MainVariables
+    [HideInInspector]
+    public float workers;
+    [HideInInspector]
+    public int roundedWorkers;
+    [HideInInspector]
+    public int freeWorkers;
+    [HideInInspector]
+    public float workerGrowth;
+    [HideInInspector]
+    public float workerGrowthInPercent;
+    [HideInInspector]
+    public float workerGrowthModifyer;
+    //subvariables
+    [HideInInspector]
+    public float workerPWNormal;
+    [HideInInspector]
+    public float workerPWMiddle;
+    [HideInInspector]
+    public float workerPWHigh;
+
+    [HideInInspector]
+    public float faithPWWMiddle;
+    [HideInInspector]
+    public float workerPWWHigh;
+
+
+    //Stellschrauben
+    public int startWorker;
+    public float standardWorkerGrowth;
+    [Space(10)]
+    public float standardWorkerPWNormal;
+    public float standardWorkerPWMiddle;
+    public float standardWorkerPWHigh;
+    [Space(10)]
+    public float standardFaithPWWMiddle;
+    public float standardWatchscoreGrowthPWWHigh;
+
+    //Singelton Awake
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public void Initialise()
+    {
+        //Faith
+  
+        faith=0;
+        faithGrowth = 0;
+        faithGrowthModifyer = 1;
+        uFaith =0;
+        faithPW=standardFaithPW;
+        faithPerSacrifice=standardFaithPerSacrifice;
+        watchscoreGrowthPerSacrifice=standardWatchscoreGrowthPerSacrifice;
+
+        //Watchscore
+
+        watchscore=0;
+        watchscoreGrowth=standardWatchscoreGrowth;
+        watchscoreGrowthModifyer = 1;
+        moneyLoss=0;
+
+        workerKillRate=0;
+        killRateCounter=0;
+
+        watchscoreDividerNormal=standardWatchscoreDividerNormal;
+        watchscoreDividerMiddle = standardWatchscoreDividerMiddle;
+        watchscoreDividerHigh = standardWatchscoreDividerHigh;
+
+        moneyLossDividerMiddle=standardMoneyLossDividerMiddle;
+
+        wKillrateDividerHigh=standardWKillrateDividerHigh;
+        kostPerMilitaWorker=standardKostPerMilitaWorker;
+
+        //Money
+
+        money=startMoney;
+        roundedMoney=(int)money;
+        moneyGrowth=0;
+        moneyGrowthModifyer = 1;
+
+        moneyPWNormal=standardMoneyPWNormal;
+        moneyPWMiddle=standardMoneyPWMiddle;
+        moneyPWHigh= standardMoneyPWHigh;
+
+        faithPMWMiddle=standardFaithPMWMiddle;
+        watchscoreGrowthPMWHigh=standardwatchscoreGrowthPMWHigh;
+
+        //Worker
+
+        workers=startWorker;
+        roundedWorkers=(int)workers;
+        freeWorkers=roundedWorkers;
+        workerGrowth=standardWorkerGrowth;
+        workerGrowthInPercent=(int)(workerGrowth*100);
+        workerGrowthModifyer = 1;
+
+        workerPWNormal=standardWorkerPWNormal;
+        workerPWMiddle = standardWorkerPWMiddle;
+        workerPWHigh = standardWorkerPWHigh;
+
+        faithPWWMiddle=standardFaithPWWMiddle;
+        workerPWWHigh=standardWatchscoreGrowthPWWHigh;
+
+    }
+
+    public void UpdateRessources()
+    {
+        faithGrowth = faithGrowthModifyer * (
+            faithPW * wm.faithWorker +
+            faithPMWMiddle * wm.moneyWorkerMiddle +
+            faithPWWMiddle * wm.watchscoreWorkerMiddle);
+    }
+
+
+}
---
+++ Assets/Scripts/Manager/WorkerManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerManager : MonoBehaviour
+{
+    [HideInInspector]
+    public int faithWorker;
+
+    [HideInInspector]
+    public int watchscoreWorkerNormal;
+    [HideInInspector]
+    public int watchscoreWorkerMiddle;
+    [HideInInspector]
+    public int watchscoreWorkerHigh;
+
+    [HideInInspector]
+    public int moneyWorkerNormal;
+    [HideInInspector]
+    public int moneyWorkerMiddle;
+    [HideInInspector]
+    public int moneyWorkerHigh;
+
+    [HideInInspector]
+    public int workerWorkerNormal;
+    [HideInInspector]
+    public int workerWorkerMiddle;
+    [HideInInspector]
+    public int workkrWorkerHigh;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
