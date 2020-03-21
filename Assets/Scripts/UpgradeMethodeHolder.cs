d2678130c1b003ff0f2c4baaf0b48ff9a4f5eaf2
Tankbart <leif-eric.nordmann@web.de>
2020-03-21 20:03:32 +0100

    DerHimmelHatseineTage

--- Assets/Scripts/UpgradeMethodeHolder.cs
+++ Assets/Scripts/UpgradeMethodeHolder.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class UpgradeMethodeHolder : MonoBehaviour
 {
+    public static GameObject bloodRainObjekt;
+    public static Material background;
+    public static Color redTint;
+
     public static void BloodRain()
     {
+        bloodRainObjekt.SetActive(true);
+        background.color = redTint;
+        //Sound?
         Debug.Log("open the gates");
     }
 }
